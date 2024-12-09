55d828e4f6b0dfd157d6d41907d6528820475b5a
igor <igoral2080@gmail.com>
2024-12-09 22:27:52 +0200

    Add info support for values bar

--- Data/Utils.cs
+++ Data/Utils.cs
@@ -4,15 +4,32 @@
     {
         public enum HealthScore
         {
-            Bad = 0,
-            Acceptable = 1,
-            Healthy = 2
+            bad = 0,
+            normal = 1,
+            good = 2
         }
 
-        public static bool EqualMonths(this DateTime dt1,  DateTime dt2)
+        public static bool EqualMonths(this DateTime dt1, DateTime dt2)
         {
-            return dt1.Year == dt2.Year && dt1.Month==dt2.Month;
+            return dt1.Year == dt2.Year && dt1.Month == dt2.Month;
         }
+
+        public static float GetPercentage(float first, float second, float third, float fourth, float current)
+        {
+            float percent = 10;
+            if (current <= first)
+                return percent;
+            if (current <= second)
+                return percent * 3;
+            if (current >= second && current <= third)
+                return percent * 5;
+            if (current <= fourth)
+                return percent * 7;
+
+            else return percent * 9;
+
+        }
+
 
         public static List<List<Message>> GetMessagesByThread(List<Message> messages)
         {
@@ -53,7 +70,7 @@
                 (int)GetInsulinScore(insulin) +
                 (int)GetCGMScore(CGM) +
                 (int)GetTriglycerideScore(triglyceride) +
-                (int)GetBloodPressureeScore(bloodPressureUpper, bloodPressureLower) +
+                (int)GetBloodPressureScore(bloodPressureUpper, bloodPressureLower) +
                 (int)GetHDLCholesterolScore(HDLCholesterol) +
                 (int)GetLDLCholesterolScore(LDLCholesterol) +
                 (int)GetRestingHeartRateScore(restingHeartRate);
@@ -67,125 +84,126 @@
         public static HealthScore GetBMIScore(float height, float weight)
         {
             if (height == -1 || weight == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
             var BMI = GetBMI(height, weight);
-            if (BMI >= 18.5 && BMI < 25)
-                return HealthScore.Healthy;
-            else if (BMI >= 25 && BMI <= 30)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+            if (BMI >= 18.5 && BMI <= 25)
+                return HealthScore.good;
+            else if ((BMI > 25 && BMI <= 28) || (BMI >= 15 && BMI < 18.5))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
         public static HealthScore GetWaistScore(ApplicationUser user, float waistSize)
         {
             if (waistSize == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
             float score = waistSize / user.Height;
 
             if (user.Gender.Equals("male", StringComparison.CurrentCultureIgnoreCase))
             {
                 if (score >= 0.43 && score <= 0.5)
-                    return HealthScore.Healthy;
-                else if (score > 0.5 && score <= 0.55)
-                    return HealthScore.Acceptable;
-                return HealthScore.Bad;
+                    return HealthScore.good;
+                else if ((score > 0.5 && score <= 0.55) || (score >= 0.38 && score < 0.43))
+                    return HealthScore.normal;
+                return HealthScore.bad;
             }
             else//female
             {
                 if (score >= 0.42 && score <= 0.48)
-                    return HealthScore.Healthy;
-                else if (score > 0.48 && score <= 0.55)
-                    return HealthScore.Acceptable;
-                return HealthScore.Bad;
+                    return HealthScore.good;
+                else if ((score > 0.48 && score <= 0.55) || (score >= 0.35 && score < 0.42))
+                    return HealthScore.normal;
+                return HealthScore.bad;
             }
         }
 
         public static HealthScore GetInsulinScore(float insulin)
         {
             if (insulin == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
             if (insulin >= 2.6 && insulin <= 5)
-                return HealthScore.Healthy;
-            else if (insulin > 5 && insulin <= 11)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+                return HealthScore.good;
+            else if ((insulin > 5 && insulin <= 11) || (insulin >= 1.5 && insulin < 2.6))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
         public static HealthScore GetCGMScore(float CGM)
         {
             if (CGM == -1)
-                return HealthScore.Bad;
-            if (CGM >= 70 && CGM <= 100)
-                return HealthScore.Healthy;
-            else if (CGM > 100 && CGM <= 125)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+                return HealthScore.bad;
+            if (CGM >= 75 && CGM <= 100)
+                return HealthScore.good;
+            else if ((CGM > 100 && CGM <= 105) || (CGM >= 70 && CGM < 75))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
         public static HealthScore GetTriglycerideScore(float triglyceride)
         {
             if (triglyceride == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
             if (triglyceride >= 50 && triglyceride <= 180)
-                return HealthScore.Healthy;
-            else if (triglyceride > 180 && triglyceride <= 200)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+                return HealthScore.good;
+            else if ((triglyceride > 180 && triglyceride <= 200) || (triglyceride >= 45 && triglyceride < 50))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
-        public static HealthScore GetBloodPressureeScore(float bloodPressureUpper, float bloodPressureLower)
+        public static HealthScore GetBloodPressureScore(float bloodPressureUpper, float bloodPressureLower)
         {
             if (bloodPressureLower == -1 || bloodPressureUpper == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
-            if (bloodPressureUpper <= 120 && bloodPressureLower <= 80)
-                return HealthScore.Healthy;
-            else if ((bloodPressureUpper > 120) && (bloodPressureUpper <= 130) && (bloodPressureLower > 80) && (bloodPressureLower <= 90))
+            if ((bloodPressureUpper <= 120 && bloodPressureUpper >= 105) && (bloodPressureLower <= 80 && bloodPressureLower >= 70))
+                return HealthScore.good;
+            else if (((bloodPressureUpper > 120) && (bloodPressureUpper <= 130) && (bloodPressureLower > 80) && (bloodPressureLower <= 90)) ||
+                ((bloodPressureUpper >= 100) && (bloodPressureUpper < 105) && (bloodPressureLower >= 60) && (bloodPressureLower < 70)))
             {
-                return HealthScore.Acceptable;
+                return HealthScore.normal;
             }
 
-            return HealthScore.Bad;
+            return HealthScore.bad;
         }
 
         public static HealthScore GetHDLCholesterolScore(float HDLCholesterol)
         {
             if (HDLCholesterol == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
-            if (HDLCholesterol >= 60)
-                return HealthScore.Healthy;
-            else if (HDLCholesterol >= 40 && HDLCholesterol <= 59)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+            if (HDLCholesterol >= 50 && HDLCholesterol <= 60)
+                return HealthScore.good;
+            else if ((HDLCholesterol >= 40 && HDLCholesterol < 50) || (HDLCholesterol > 60 && HDLCholesterol <= 70))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
         public static HealthScore GetLDLCholesterolScore(float LDLCholesterol)
         {
             if (LDLCholesterol == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
-            if (LDLCholesterol < 100)
-                return HealthScore.Healthy;
-            else if (LDLCholesterol >= 100 && LDLCholesterol <= 160)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+            if (LDLCholesterol >= 100 && LDLCholesterol <= 130)
+                return HealthScore.good;
+            else if ((LDLCholesterol > 130 && LDLCholesterol <= 160) || (LDLCholesterol >= 70 && LDLCholesterol < 100))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
         public static HealthScore GetRestingHeartRateScore(float restingHeartRate)
         {
             if (restingHeartRate == -1)
-                return HealthScore.Bad;
+                return HealthScore.bad;
 
             if (restingHeartRate >= 40 && restingHeartRate <= 60)
-                return HealthScore.Healthy;
-            else if (restingHeartRate > 60 && restingHeartRate <= 80)
-                return HealthScore.Acceptable;
-            return HealthScore.Bad;
+                return HealthScore.good;
+            else if ((restingHeartRate > 60 && restingHeartRate <= 80) || (restingHeartRate >= 35 && restingHeartRate < 40))
+                return HealthScore.normal;
+            return HealthScore.bad;
         }
 
     }
