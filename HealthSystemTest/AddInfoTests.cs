1c5debb8941db687718f896495a557c58e734bd9
igor <igoral2080@gmail.com>
2024-12-24 18:32:32 +0200

    add testing capabilities that include users in components

--- HealthSystemTest/AddInfoTests.cs
+++ HealthSystemTest/AddInfoTests.cs
@@ -1,8 +1,26 @@
-﻿
+﻿using HealthSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HealthSystem.Components.Pages;
+using Moq;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
 namespace HealthSystemTest
 {
     public class AddInfoTests : TestContext
     {
+        [Fact]
+        public void IsDataAccurateFromDB()
+        {
+            Globals.InitializeUserData(this, Services);
+
+            var cut = RenderComponent<AddInfo>();
+
+            var result = cut.FindAll(".info-row");
+
+            result.MarkupMatches("");
+        }
+
         //[Fact]
         //public void CounterStartsAtZero()
         //{
@@ -12,5 +30,6 @@ namespace HealthSystemTest
         //    // Assert that content of the paragraph shows counter at zero
         //    cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
         //}
+
     }
 }
---
+++ HealthSystemTest/Globals.cs
@@ -0,0 +1,47 @@
+﻿using HealthSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemTest
+{
+    public static class Globals
+    {
+        public const string AdminId = "64730bef-13d9-406d-8a78-080dbfabd411";
+        public const string ConnectionString = @"Data Source=Data/HealthSysem.db";
+
+        public static void InitializeUserData(TestContext self, TestServiceProvider Services)
+        {
+            Services.AddDbContextFactory<ApplicationDbContext>(options =>
+                options.UseSqlite(Globals.ConnectionString));
+            Services.AddCascadingAuthenticationState();
+            Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+                .AddIdentityCookies();
+            var mockUserManager = MockUserManager<ApplicationUser>.Create();
+            mockUserManager
+                .Setup(um => um.FindByIdAsync(Globals.AdminId))
+                .ReturnsAsync(new ApplicationUser { Id = Globals.AdminId, UserName = "Admin" });
+            var userManagerMock = mockUserManager.Object;
+            Services.AddScoped(_ => mockUserManager.Object);
+
+            var authContext = self.AddTestAuthorization();
+            authContext.SetAuthorizing();
+            authContext.RegisterAuthorizationServices(Services);
+            authContext.SetAuthorized("Admin");
+            authContext.SetRoles("Admin");
+            authContext.SetClaims(
+                new System.Security.Claims.Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", AdminId)
+
+                );
+        }
+    }
+}
