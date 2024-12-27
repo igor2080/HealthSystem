d749ba6864cb65d9258fb023270a97ced81f0d15
igor <igoral2080@gmail.com>
2024-12-27 13:11:57 +0200

    add metabolic health to addinfo page, visual changes to front page and addinfo page

---
+++ HealthSystemTest/StatisticsTests.cs
@@ -0,0 +1,22 @@
+﻿using HealthSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HealthSystem.Components.Pages;
+using Moq;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace HealthSystemTest
+{
+    public class StatisticsTests : TestContext
+    {
+        [Fact]
+        public void IsFileDownloadCorrect()
+        {
+            Globals.InitializeUserData(this,Services);
+            /////////////////
+        }
+    }
+}
